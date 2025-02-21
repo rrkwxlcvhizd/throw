f0d5127766f4684c0f1dc0ec9b081d3ec6f3357d
rrkwxlcvhizd <rrkwxlcvhizd@gmail.com>
2025-02-21 11:42:11 +0800

    Modify wide.cpp

--- wide.cpp
+++ wide.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# On recognize whole space two billion.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
